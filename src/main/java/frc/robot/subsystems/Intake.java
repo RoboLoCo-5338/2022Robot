77ba9e2a2d7d20aca3f0d161b0f86fed9be79c4a
mooshroom_person <nithya.vinodh14@gmail.com>
2022-02-05 13:57:28 -0500

    Initial Commit

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,18 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+public class Intake {
+   //pneumatics on the right
+   //pneumatics on the left
+   //both need to go at the same time
+   //motor, starts when the pneumatics start
+   private DoubleSolenoid rightSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM,1,2);// right solenoid object, ports are placeholders
+   private DoubleSolenoid leftSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM,3,4);//left solenoid object, ports are placeholders
+   private WPI_TalonFX intakeMotor = new WPI_TalonFX(2);
+    
+   rightSolenoid.set(kOff);
+
+}
