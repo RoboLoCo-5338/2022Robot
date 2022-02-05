bca540f2c789e71d5fda48afa8c736e28be0da99
abbymartin <abbyvmartin51@gmail.com>
2022-02-05 16:14:11 -0500

    cleaned up code

--- src/main/java/frc/robot/subsystems/ShooterSystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSystem.java
@@ -6,25 +6,22 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 
 public class ShooterSystem extends SubsystemBase{
- private WPI_TalonFX shooterMotor = new WPI_TalonFX(0);
- private final double MAX_OUTPUT = 0.5;
+ 
+  private WPI_TalonFX shooterMotor;
+  private final double MAX_OUTPUT = 0.5;
  
   public ShooterSystem() {
-
+    shooterMotor = new WPI_TalonFX(0);
     this.shooterMotor.setNeutralMode(NeutralMode.Brake);       
   }
-
 
   public void shoot(){
     this.shooterMotor.set(ControlMode.PercentOutput, MAX_OUTPUT);    
   }
-
 
   public void stopShoot(){
     this.shooterMotor.set(ControlMode.PercentOutput, 0);    
