bb90bdc44dfa4274b8dd58806724264971a205be
mooshroom_person <nithya.vinodh14@gmail.com>
2022-02-06 11:46:39 -0500

    Intake Subsytem (now with comments!)

--- src/main/java/frc/robot/commands/IntakeCommands.java
+++ src/main/java/frc/robot/commands/IntakeCommands.java
@@ -1,33 +1,32 @@
 package frc.robot.commands;
-
+//imports all the necessary classes
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.robot.RobotContainer;
 
 public class IntakeCommands {
-
-    public static Command extendIntakeCommand() {
+    public static Command extendIntakeCommand() {//creates the extend intake command
         return new RunCommand(
 			() -> RobotContainer.intake.extendIntake(),
 			RobotContainer.intake
 		);
     }  
 
-    public static Command extendOutakeCommand() {
+    public static Command extendOutakeCommand() {//creates the extend outake command
         return new RunCommand(
 			() -> RobotContainer.intake.extendOutake(),
 			RobotContainer.intake
 		);
     }  
 
-    public static Command retractIntakeCommand() {
+    public static Command retractIntakeCommand() {//creates the retract intake command
         return new RunCommand(
 			() -> RobotContainer.intake.extendOutake(),
 			RobotContainer.intake
 		);
     }  
 
-    public static Command retractOutakeCommand() {
+    public static Command retractOutakeCommand() {//creates the retract outake command
         return new RunCommand(
 			() -> RobotContainer.intake.extendOutake(),
 			RobotContainer.intake
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,38 +1,36 @@
 package frc.robot.subsystems;
-
+//imports all the necessary bits 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
 import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 
 public class Intake extends SubsystemBase{
-   private static DoubleSolenoid rightSolenoid;
+   private static DoubleSolenoid rightSolenoid;//creates the solenoid objects and the motor object
    private static DoubleSolenoid leftSolenoid;
    private static WPI_TalonFX intakeMotor;
 
    public Intake(){
-   //pneumatics on the right and left
-   //both need to go at the same time
-   //motor, starts when the pneumatics start
+   /* pneumatics on the right and left go at the same time
+   motor starts when the pneumatics start, might want to turn it off during climb?
+   */
       DoubleSolenoid rightSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM,1,2);// right solenoid object, ports are placeholders
       DoubleSolenoid leftSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM,3,4);//left solenoid object, ports are placeholders
       WPI_TalonFX intakeMotor = new WPI_TalonFX(2);
    }
     
    public void extendIntake(){
-      //the controller name and button are placeholders
          rightSolenoid.set(kForward);
          leftSolenoid.set(kForward); 
-         intakeMotor.set(ControlMode.PercentOutput, 0.5);//percentOutput is a placeHolder    
+         intakeMotor.set(ControlMode.PercentOutput, 0.5);//percentOutput is a placeholder    
    }
 
    public void extendOutake(){
       rightSolenoid.set(kForward);
       leftSolenoid.set(kForward); 
-      intakeMotor.set(ControlMode.PercentOutput, -0.5);//percentOutput is a placeHolder
+      intakeMotor.set(ControlMode.PercentOutput, -0.5);//percentOutput is a placeholder
    }
 
    public void retractIntake(){
