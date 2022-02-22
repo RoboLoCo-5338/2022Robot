ad00b850e6d1f6c89a3ae725be3ea3831158f960
Asvaliveti <asvaliveti@gmail.com>
2022-02-21 22:58:55 -0500

    changed run commands to instant commands

--- src/main/java/frc/robot/commands/ClimbCommands.java
+++ src/main/java/frc/robot/commands/ClimbCommands.java
@@ -6,27 +6,28 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.robot.RobotContainer;
 
 /** Add your docs here. */
 public class ClimbCommands {
-    public static Command longForward = new RunCommand(
+    public static Command longForward = new InstantCommand(
       () -> RobotContainer.climb.longForward(),
       RobotContainer.climb
     );
 
-    public static Command longReverse = new RunCommand(
+    public static Command longReverse = new InstantCommand(
       () -> RobotContainer.climb.longReverse(),
       RobotContainer.climb
     );
 
-    public static Command shortForward = new RunCommand(
+    public static Command shortForward = new InstantCommand(
       () -> RobotContainer.climb.shortForward(),
       RobotContainer.climb
     );
 
-    public static Command shortReverse = new RunCommand(
+    public static Command shortReverse = new InstantCommand(
       () -> RobotContainer.climb.shortReverse(),
       RobotContainer.climb
     );
