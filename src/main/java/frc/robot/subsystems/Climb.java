ac83cd7f4ddfad35338a9f163c0822bd81406400
Asvaliveti <asvaliveti@gmail.com>
2022-02-26 12:34:30 -0500

    removed second winch from climb

--- src/main/java/frc/robot/subsystems/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -35,7 +35,6 @@ public class Climb extends PIDSubsystem {
     /** Initialize Talons */
     private static WPI_TalonFX motor;
     private static WPI_TalonFX winch1;
-    private static WPI_TalonFX winch2;
 
     //initialize solenoids
     DoubleSolenoid longArm = new DoubleSolenoid(PneumaticsModuleType.CTREPCM,0,1);
@@ -46,7 +45,6 @@ public class Climb extends PIDSubsystem {
 
         motor = new WPI_TalonFX(Constants.CLIMB_MOTOR_ID);
         winch1 = new WPI_TalonFX(Constants.WINCH_1_ID);
-        winch2 = new WPI_TalonFX(Constants.WINCH_2_ID);
 
         configureTalon();
     }
@@ -67,14 +65,6 @@ public class Climb extends PIDSubsystem {
         winch1.setStatusFramePeriod(StatusFrame.Status_1_General, 5, 0);
         winch1.setControlFramePeriod(ControlFrame.Control_3_General, 5);
         winch1.configClosedLoopPeakOutput(0, PEAK_OUTPUT, 100);
-    
-        winch2.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, 100);
-        winch2.setNeutralMode(NeutralMode.Brake);
-        winch2.follow(winch1);
-        winch2.configNeutralDeadband(0.001, 0);
-        winch2.setStatusFramePeriod(StatusFrame.Status_1_General, 5, 0);
-        winch2.setControlFramePeriod(ControlFrame.Control_3_General, 5);
-        winch2.configClosedLoopPeakOutput(0, PEAK_OUTPUT, 100);
     }
     public void setPIDF(double kP, double kI, double kD, double kF) {
         motor.config_kP(0, kP, 100);
@@ -84,10 +74,6 @@ public class Climb extends PIDSubsystem {
         winch1.config_kP(0, kP, 100);
         winch1.config_kI(0, kI, 100);
         winch1.config_kD(0, kD, 100);
-        
-        winch2.config_kP(0, kP, 100);
-        winch2.config_kI(0, kI, 100);
-        winch2.config_kD(0, kD, 100);
     }
     
     public void climbToPos(double pos){
@@ -104,12 +90,10 @@ public class Climb extends PIDSubsystem {
 
     public void winchToPos(double pos){
         winch1.set(ControlMode.Position, pos);
-        winch2.follow(winch1);
     }
 
     public void winchPercent(double speed) {
         winch1.set(ControlMode.PercentOutput, speed);
-        winch2.follow(winch1);
     }
 
     public double getWinchPosition() {
